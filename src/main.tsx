51c27730fc31febc28c84457ccfa07da8fc45686
diwka <dkasymov421@gmail.com>
2026-03-25 18:12:40 +0500

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import "./styles/index.css";
+
+import App from "./app/App";
+
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error("Root element '#root' not found");
+}
+
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
